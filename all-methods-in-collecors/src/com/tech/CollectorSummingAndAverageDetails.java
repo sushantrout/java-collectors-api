770b7ce88edb4611314f8aae4da1b0e45559c1cb
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2022-08-14 21:59:14 +0530

    suming and average

--- all-methods-in-collecors/src/com/tech/CollectorSummingAndAverageDetails.java
+++ all-methods-in-collecors/src/com/tech/CollectorSummingAndAverageDetails.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class CollectorSummingDetails {
+public class CollectorSummingAndAverageDetails {
 	public static void main(String[] args) {
 
 		List<Employee> employees = EmployeeProvider.getEmployees();
@@ -21,9 +21,30 @@ public class CollectorSummingDetails {
 		 * summingDouble(java.util.function.ToDoubleFunction);
 		 */
 		
+		System.out.println("Summing");
+		  
 		Map<String, Integer> collect = employees.stream()
 				.collect(Collectors.groupingBy(Employee::getDepartment, Collectors.summingInt(Employee::getAge)));
 		collect.forEach((k, v) -> {
+			System.out.println(k+" => " +v);
+		});
+		
+		
+		/*
+		 * public static java.util.stream.Collector
+		 * averagingInt(java.util.function.ToIntFunction);
+		 * 
+		 * public static java.util.stream.Collector
+		 * averagingLong(java.util.function.ToLongFunction);
+		 * 
+		 * public static java.util.stream.Collector
+		 * averagingDouble(java.util.function.ToDoubleFunction);
+		 */
+		
+		System.out.println("Average");
+		Map<String, Double> averages = employees.stream()
+				.collect(Collectors.groupingBy(Employee::getDepartment, Collectors.averagingInt(Employee::getAge)));
+		averages.forEach((k, v) -> {
 			System.out.println(k+" => " +v);
 		});
 	}
